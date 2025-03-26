1b41ab1fde0539a3a8a480aff6660599d10b0fb8
voiser-rgb <voiseram@gmail.com>
2025-03-26 12:04:20 +0300

    20.add figure tag

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,8 +6,13 @@ import img from "../../img/TheCarinaNebula.jpg";
 const Profile = () => {
 	return <div className={styles.content}>
 		<div>
-			<img className={styles.image} src={img} alt="NGC3372" />
-			<figcaption>The Carina Nebula, ESO 128-EN13.It is located at a distance of 6500 to 10,000 light-years from Earth.</figcaption>
+			<figure>
+				<img className={styles.image} src={img} alt="NGC3372"/>
+				<figcaption>The Carina Nebula, ESO 128-EN13.It is located at a distance of 6500 to 10,000 light-years
+					from
+					Earth.
+				</figcaption>
+			</figure>
 		</div>
 		<div>
 			ava + description
