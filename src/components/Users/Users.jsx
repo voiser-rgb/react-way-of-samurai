900ec0a3e40f6b825e1e0566d40ec50acd0d28a4
voiser-rgb <voiseram@gmail.com>
2025-09-23 15:13:40 +0300

    52.Side Effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,38 +4,17 @@ import axios from "axios";
 import userPhoto from "../../assets/images/anonymous .png"
 
 const Users = (props) => {
-	console.log(props.users);
-	if (props.users.length === 0) {
-		axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
-			props.setUsers(response.data.items);
-		})
-		// props.setUsers(
-		// 	[
-		// 	{
-		// 		id: 1,
-		// 		photoUrl: "https://www.kino-teatr.ru/acter/album/7453/649586.jpg",
-		// 		followed: false,
-		// 		fullName: "Andrey",
-		// 		status: "Just do it!",
-		// 		location: {country: "Russia", city: "Moscow"}
-		// 	}, {
-		// 		id: 2,
-		// 		photoUrl: "https://vz.ua/static/image/big/1390848957.jpg",
-		// 		followed: true,
-		// 		fullName: "Alex",
-		// 		status: "We're all friends.",
-		// 		location: {country: "USA", city: "New-York"}
-		// 	}, {
-		// 		id: 3,
-		// 		photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Feszty_%C3%81rp%C3%A1d_%C3%81rp%C3%A1d_vez%C3%A9r.jpg/330px-Feszty_%C3%81rp%C3%A1d_%C3%81rp%C3%A1d_vez%C3%A9r.jpg",
-		// 		followed: false,
-		// 		fullName: "Árpád",
-		// 		status: "I am hungry",
-		// 		location: {country: "Hungary", city: "Budapest"}
-		// 	},
-		// ]);
+	const getUsers = () => {
+		if (props.users.length === 0) {
+			console.log(props);
+			axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
+				props.setUsers(response.data.items);
+			})
+		}
 	}
+
 	return (<div className={styles.wrapper}>
+		<button onClick={getUsers}>Get users</button>
 		{props.users.map((user) => <div className={styles.item} key={user.id}>
 			{/*Блок для Аватара и для кнопки Followed*/}
 			<div className={styles.userBlock}>
